6a0ca94b81e7224e67cf0bf2cfb238f25166c524
Javeed Shariff <96673407+shariffjaveed@users.noreply.github.com>
2022-01-21 16:10:38 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class College {
+	public void collegeName()
+	{
+		System.out.println("Guru nanak College of arts and science, chennai");
+	}
+	
+	public void collegeCode()
+	{
+		System.out.println("PGP College code '88' ");
+	}
+	
+	public void collegeRank()
+	{
+		System.out.println("PGP College Rank No.1");
+	}
+}
