9cff3d5c8b7e2fa74acc8f1e2c68496bc4f8b12c
Javeed Shariff <96673407+shariffjaveed@users.noreply.github.com>
2022-01-22 15:36:09 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class BankInfo {
+	public  void saving() 
+	{
+		System.out.println("Welcome to the savings account portal");
+	}
+	
+	public  void fixed() 
+	{
+		System.out.println("The Fixed deposit starts from 500 per month");
+	}
+	
+	public  void deposit() 
+	{
+		System.out.println("Mininum deposit amount for the savings account is 1000");
+	}
+}
